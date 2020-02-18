4ddf683bf1fd6657b0639fb5415a59f57c9fbf0a
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-02-18 19:46:57 +0000

    mainly just amended syntax errors. Have not successfully got leaderboard working

--- clickGame/index.js
+++ clickGame/index.js
@@ -1,74 +1,75 @@
 
-    let clickToPlay = document.getElementById('click-to-play');
-    let clickBtn = document.getElementById('record-click');
-    let scoreTotal = document.getElementById('score');
-    let timer = document.getElementById('timer');
-    let endGameBox = document.getElementById('end-game-box');
-    let leaderBoard = document.getElementById('leader-board');
+let clickToPlay = document.getElementById('click-to-play');
+let clickBtn = document.getElementById('record-click');
+let scoreTotal = document.getElementById('score');
+let timer = document.getElementById('timer');
+let endGameBox = document.getElementById('end-game-box');
+let leaderBoard = document.getElementById('leader-board');
 
-    //if clicked, display press me button
-    clickToPlay.addEventListener('click', () => {
-        clickBtn.classList.add('record-click');
-        clickBtn.classList.remove('preClick');
-        endGameBox.classList.remove('end-game-box');
-        endGameBox.classList.add('end-game-box-start');
-        endGameBox.classList.remove('page-load-gif');
-    });
+//if clicked, display press me button
+clickToPlay.addEventListener('click', () => {
+    clickBtn.classList.add('record-click');
+    clickBtn.classList.remove('preClick');
+    endGameBox.classList.remove('end-game-box');
+    endGameBox.classList.add('end-game-box-start');
+    endGameBox.classList.remove('page-load-gif');
+});
 
-    //create event listener to listen to clicks and + 1 each click
+//create event listener to listen to clicks and + 1 each click
 
-    var counter = 0;
-    function recordClick() {
-            counter++;
-            scoreTotal.innerHTML = `<p>SCORE</p><p>${counter}</p>`;
+var counter = 0;
+function recordClick() {
+    counter++;
+    scoreTotal.innerHTML = `<p>SCORE</p><p>${counter}</p>`;
+}
+
+clickBtn.addEventListener('click', recordClick);
+
+var seconds = 6;
+var interval;
+
+if(seconds === 0) {
+    seconds = 6;
+}
+
+function timerFunction() {
+    interval = setInterval(() => {
+        seconds --;
+        timer.innerHTML = `<p>TIMER</p><p>${seconds}</p>`;
+        if(!seconds){
+            clearInterval(interval);
+            clickBtn.removeEventListener('click', recordClick);
+            clickBtn.classList.add('preClick');
+            clickBtn.classList.remove('record-click');
+            seconds = 6;
+            scoreTotal.innerHTML = `<p>SCORE</p><p>finished!</p>`;
+            counter = 0;
+            clickBtn.addEventListener('click', recordClick);
+            endGameBox.classList.remove('end-game-box-start');
+            endGameBox.classList.add('end-game-box');
+            leaderBoard.dispatchEvent(roundOver)
         }
-    clickBtn.addEventListener('click', recordClick);
+    }, 1000)
+}
 
-    var seconds = 6;
-    var interval;
+//when record-click pressed, start timer & display live score
+let timerFun = clickToPlay.addEventListener('click', () => {
+    timerFunction();
+    document.getElementById('click-to-play').removeEventListener('click', timerFun)
+});
 
-    if(seconds === 0) {
-        seconds = 6;
-    }
+//when timer reaches 0, display name and score in leader-board
+var roundOver = new Event('roundOver', {
+    bubbles: true,
+    cancelable: true
+});
 
-    function timerFunction() {
-        interval = setInterval(() => {
-            seconds --;
-            timer.innerHTML = `<p>TIMER</p><p>${seconds}</p>`;
-            if(!seconds){
-                clearInterval(interval);
-                clickBtn.removeEventListener('click', recordClick);
-                clickBtn.classList.add('preClick');
-                clickBtn.classList.remove('record-click');
-                seconds = 6;
-                scoreTotal.innerHTML = `<p>SCORE</p><p>finished!</p>`;
-                counter = 0;
-                clickBtn.addEventListener('click', recordClick);
-                endGameBox.classList.remove('end-game-box-start');
-                endGameBox.classList.add('end-game-box');
-                leaderBoard.dispatchEvent(roundOver)
-            }
-        }, 1000)
-    }
-
-    //when record-click pressed, start timer & display live score
-    let timerFun = clickToPlay.addEventListener('click', () => {
-        timerFunction();
-        document.getElementById('click-to-play').removeEventListener('click', timerFun)
-    });
-
-    //when timer reaches 0, display name and score in leader-board
-    var roundOver = new CustomEvent('roundOver', {
-        bubbles: true,
-        cancelable: true,
-    });
-
-    leaderBoard.addEventListener('roundOver', () => {
-        var name = document.getElementById('name').value;
-        var score = counter
-        var recordScore = document.createElement('p');
-        recordScore.textContent = name + ': ' + score;
-        leaderBoard.appendChild(recordScore);
-    });
+leaderBoard.addEventListener('roundOver', () => {
+    var name = document.getElementById('name').value;
+    var score = counter
+    var recordScore = document.createElement('p');
+    recordScore.textContent = name + ': ' + score;
+    leaderBoard.appendChild(recordScore);
+});
 
 
