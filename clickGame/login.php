0654ca9526ee8828cf7663f1ae201a7fcb420073
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-02-22 17:03:32 +0000

    created login php file and started the html layout

---
+++ clickGame/login.php
@@ -0,0 +1,26 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>click Game</title>
+    <link rel="stylesheet" href="style.css">
+    <script defer src="index.js"></script>
+</head>
+<body>
+    <div class="login-box">
+        <form class="login" method="post">
+            <input type="text" id="login-user" placeholder="User email"/>
+            <input type="password" id="login-password" placeholder="Password"/>
+            <input type="submit" id="login-submit" placeholder="LOGIN"/>
+        </form>
+        <div
+    </div>
+
+
+</body>
+</html>
