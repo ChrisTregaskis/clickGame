3a9a82062ae0470810580e4688fb11f205cc2fc9
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-02-23 14:13:55 +0000

    refactered leaderboard display to use an object array instead of new custom event. Took out unused code

--- clickGame/index.js
+++ clickGame/index.js
@@ -4,7 +4,7 @@ let clickBtn = document.getElementById('record-click');
 let scoreTotal = document.getElementById('score');
 let timer = document.getElementById('timer');
 let endGameBox = document.getElementById('end-game-box');
-let leaderBoard = document.getElementById('leader-board');
+let leaderBoardContent = document.getElementById('leader-board-content');
 
 //if clicked, display press me button
 clickToPlay.addEventListener('click', () => {
@@ -46,18 +46,19 @@ function timerFunction() {
             clickBtn.classList.add('preClick');
             clickBtn.classList.remove('record-click');
             seconds = 6;
-            // scoreTotal.textContent = 'finished!';
             counter = 0;
             clickBtn.addEventListener('click', recordClick);
             endGameBox.classList.remove('end-game-box-start');
             endGameBox.classList.add('end-game-box');
-            leaderBoard.dispatchEvent(gameOver)
+            var name = document.getElementById('name').value;
+            var score = scoreTotal.textContent;
+            fillLeaderBoard(name, score);
+            console.log(leaderBoardArray);
         }
     }, 1000)
 }
 
 //when record-click pressed, start timer & display live score
-
 let timerFun = clickToPlay.addEventListener('click', () => {
     if(document.getElementById('name').value.length !== 0) {
         timerFunction();
@@ -66,17 +67,13 @@ let timerFun = clickToPlay.addEventListener('click', () => {
 });
 
 //when timer reaches 0, display name and score in leader-board
-var gameOver = new CustomEvent('roundOver', {
-    bubbles: true,
-    cancelable: true
-});
+var leaderBoardArray = {};
 
-leaderBoard.addEventListener('roundOver', () => {
-    var name = document.getElementById('name').value;
-    var score = scoreTotal.textContent;
-    var recordScore = document.createElement('p');
-    recordScore.textContent = name + ': ' + score;
-    leaderBoard.appendChild(recordScore);
-});
-
-
+function fillLeaderBoard(roundName, roundScore) {
+    leaderBoardArray[roundName] = roundScore;
+    var playerScoreDisplay = document.createElement('p');
+    Object.keys(leaderBoardArray).forEach((key, item) => {
+        playerScoreDisplay.textContent = key + ': ' + leaderBoardArray[key]
+        leaderBoardContent.appendChild(playerScoreDisplay)
+    })
+}
